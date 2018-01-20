d649ad730ad40e10b8f83eef1d2e86bcf005cdbb
CoderNice <ycbnice@gmail.com>
2018-01-20 11:19:06 +0800

    hsv初步改动
    hsv初步改动

--- opcv_yuchenbing/Develop/develop/ero_dila.py
+++ opcv_yuchenbing/Develop/develop/ero_dila.py
@@ -9,22 +9,38 @@ import numpy as np
 
 im = cv2.imread('thumb.jpg')
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
-image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-image = cv2.drawContours(image, contours, 2, (0,0,255), 3)
-kernel = np.ones((5,5),np.uint8)
-erosion = cv2.erode(image,kernel,iterations = 1)
-dilation = cv2.dilate(erosion,kernel,iterations = 1)
-titles = ['Original Image','erosion','dilation']
-images = [image, erosion, dilation]
-cv2.imshow('orginal',image)
-cv2.imshow('erosion',erosion)
-cv2.imshow('dilation',dilation)
-#for i in range(3):
+ret,th1 = cv2.threshold(imgray,127,255,cv2.THRESH_BINARY)
+#image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+th2 = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+#阈值取自相邻区域的平均值
 
- #   plt.subplot(1,3,i+1),plt.imshow(images[i],'gray')
-#    plt.title(titles[i])
-#    plt.xticks([]),plt.yticks([])
-#plt.show()
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+th3 = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+cv2.THRESH_BINARY,11,2)
+#阈值取值相邻区域的加权和，权重为一个高斯窗口。
+#11 为Block size, 2 为C 值
+
+
+
+#image = cv2.drawContours(image, contours, 2, (0,255,0), 3)
+
+
+#腐蚀#膨胀
+kernel = np.ones((5,5),np.uint8)
+
+
+erosion1 = cv2.erode(th1,kernel,iterations = 1)
+dilation1 = cv2.dilate(erosion1,kernel,iterations = 1)
+erosion2 = cv2.erode(th2,kernel,iterations = 1)
+dilation2 = cv2.dilate(erosion2,kernel,iterations = 1)
+erosion3 = cv2.erode(th3,kernel,iterations = 1)
+dilation3 = cv2.dilate(erosion3,kernel,iterations = 1)
+titles = ['original','erosion1','dilation1','erosion2','dilation2','erosion3','dilation3']
+images = [imgray, erosion1, dilation1,erosion2, dilation2,erosion3, dilation3]
+
+for i in range(7):
+
+    plt.subplot(1,7,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
--- opcv_yuchenbing/Develop/develop/hsv.py
+++ opcv_yuchenbing/Develop/develop/hsv.py
@@ -11,19 +11,34 @@ while(1):
     ret,frame=cap.read()
     # 转换到HSV
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+    #转换到GREY
+    imgray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
     # 设定蓝色的阈值
     lower_blue = np.array([0, 50, 70])
     upper_blue = np.array([50, 200, 200])
+
     # 根据阈值构建掩模
     mask=cv2.inRange(hsv,lower_blue,upper_blue)
+
+    #自适应阈值
+    #th2 = cv2.adaptiveThreshold(mask,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+
     # 对原图像和掩模进行位运算
-    res=cv2.bitwise_and(frame,frame,mask=mask)
+    #res=cv2.bitwise_and(frame,frame,mask=mask)
+
+    kernel = np.ones((5,5),np.uint8)
+
+    dilation1 = cv2.dilate(mask,kernel,iterations = 1)
+    erosion1 = cv2.erode(dilation1,kernel,iterations = 1)
+
+
+
     # 显示图像
-
-
-
     #cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
+    #cv2.imshow('ero',erosion1)
+    cv2.imshow('dila',dilation1)
     #cv2.imshow('res',res)
     k=cv2.waitKey(5)&0xFF
     if k==27:
